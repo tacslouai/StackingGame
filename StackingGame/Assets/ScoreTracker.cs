721e23c24cd0b44d89133849a854ed2eec03fc69
tacslouai <louaihammad2@gmail.com>
2020-03-13 08:41:27 -0400

    Added Scripts

---
+++ StackingGame/Assets/ScoreTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTracker : MonoBehaviour
+{
+    public ScoreTracker INSTANCE;
+    public int bestScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (INSTANCE == null)
+        {
+            INSTANCE = this;
+            bestScore = 0;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
